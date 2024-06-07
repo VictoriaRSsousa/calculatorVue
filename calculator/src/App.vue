ac48d78abaf8ec947671fec07fa85d1770acd465
VictoriaRSsousa <victoriars.sousa@gmail.com>
2024-06-07 16:56:01 -0300

    config dev

---
+++ calculator/src/App.vue
@@ -0,0 +1,22 @@
+<script  lang="ts">
+
+import Button from './components/Button.vue'
+
+export default {
+  components: {
+    Button
+  }
+}
+
+</script>
+
+<template>
+  <div class="bg-red-700">
+    <h1 >CALCULATOR</h1>
+    <Button/>
+
+
+  </div>
+</template>
+
+
---
+++ calculator/src/components/Button.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      teste: "testando componente"
+    }
+  }
+}
+</script>
+
+<template>
+    <h2>{{ teste }}</h2>
+    
+</template>
