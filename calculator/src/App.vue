5a655f9ef76afae1d57c0c09641b839ae41f5ee1
Elton Ribeiro <96088869+eltonls@users.noreply.github.com>
2024-06-10 09:50:25 -0300

    Feature/calculator (#2)
    * add: buttons
    
    * add calculator component with buttons and display
    
    ---------
    
    Co-authored-by: VictoriaRSsousa <victoriars.sousa@gmail.com>

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -1,21 +1,21 @@
-<script  lang="ts">
+<script lang="ts">
 
 import Button from './components/Button.vue'
+import Calculator from './components/Calculator.vue';
 
 export default {
-  data(){
-    return{
-       buttons: [7,8,9,'+',4,5,6,'-',1,2,3,'/',',',0,'=','*']
+  data() {
+    return {
+      buttons: [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '/', ',', 0, '=', '*']
     }
-
   },
   components: {
-    Button
+    Button,
+    Calculator
   },
   methods: {
     receiveEmit(button) {
       console.log(button);
-      
     }
   }
 }
@@ -24,15 +24,6 @@ export default {
 
 <template>
   <div class=" flex h-screen justify-center items-center">
-    <div class="grid grid-cols-4 gap-2 ">
-
-      <Button v-for=" button in buttons" :key="button" :button="button" @button="receiveEmit"/>  
-    </div>
-   
-
-
-
+    <Calculator />
   </div>
 </template>
-
-
