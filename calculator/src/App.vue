016fc38d48d24f99ecc8bf0d93be6d1b6117f206
VictoriaRSsousa <82991480+VictoriaRSsousa@users.noreply.github.com>
2024-06-10 09:15:14 -0300

    add: buttons (#1)

--- calculator/src/App.vue
+++ calculator/src/App.vue
@@ -3,17 +3,33 @@
 import Button from './components/Button.vue'
 
 export default {
+  data(){
+    return{
+       buttons: [7,8,9,'+',4,5,6,'-',1,2,3,'/',',',0,'=','*']
+    }
+
+  },
   components: {
     Button
+  },
+  methods: {
+    receiveEmit(button) {
+      console.log(button);
+      
+    }
   }
 }
 
 </script>
 
 <template>
-  <div class="bg-red-700">
-    <h1 >CALCULATOR</h1>
-    <Button/>
+  <div class=" flex h-screen justify-center items-center">
+    <div class="grid grid-cols-4 gap-2 ">
+
+      <Button v-for=" button in buttons" :key="button" :button="button" @button="receiveEmit"/>  
+    </div>
+   
+
 
 
   </div>
